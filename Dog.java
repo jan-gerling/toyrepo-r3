597a9225c4e90c431f75a2442e91429fa0d940a0
rafaeldurelli <rafael.durelli@ufla.br>
2020-02-05 09:19:13 -0300

    add field

--- Dog.java
+++ Dog.java
@@ -2,6 +2,8 @@ import inheritance.superinfo.AnimalSuper;
 
 public class Dog extends AnimalSuper {
 
+    private String bree;
+
     @Override
     public void sound(String noise) {
         System.out.println("Bark " + noise);
